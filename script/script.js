45e4808204602a5984ad409b19c6fc8b709bed23
Jimmy Wei <jimmy.t.wei@intel.com>
2025-06-28 19:24:14 -0700

    Update script.js
    Updated pipeline to point to Intel/polite-guard.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 import { env, pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers'
 
-const classifier = await pipeline('text-classification', 'model')
+const classifier = await pipeline('text-classification', 'Intel/polite-guard')
 
 async function classify(input) {
   const output = await classifier(input)
